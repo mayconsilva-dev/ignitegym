4efe0f2c658a8366ff430dd43725241d2a933c21
mayconsilva24 <sousamaycon83@gmail.com>
2025-02-18 20:10:50 -0300

    FileSystem installation

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,24 +1,74 @@
+import { useState } from "react";
 import { 
     ScrollView, 
     TouchableOpacity, 
     KeyboardAvoidingView, 
     Platform, 
     TouchableWithoutFeedback, 
-    Keyboard 
+    Keyboard, 
+    Alert
 } from "react-native";
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Text, VStack, useToast } from "@gluestack-ui/themed";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { ToastMessage } from "@components/ToastMessage";
 
 export function Profile() {
+
+    const [usePhoto, setUsePhoto] = useState("https://github.com/mayconsilva-dev.png")
+
+    const toast = useToast()
+
+    async function handleUserPhotoSelect() {
+        try {
+          const photoSelected = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            quality: 1,
+            aspect: [4, 4],
+            allowsEditing: true,
+          })
+    
+          if (photoSelected.canceled) {
+            return
+          }
+    
+          const photoUri = photoSelected.assets[0].uri
+    
+          if (photoUri) {
+            const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+              size: number
+            }
+    
+            if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+              return toast.show({
+                placement: 'top',
+                render: ({ id }) => (
+                  <ToastMessage
+                    id={id}
+                    action="error"
+                    title="Essa imagem é muito grande. Escolha uma de até 5MB"
+                    onClose={() => toast.close(id)}
+                  />
+                ),
+              })
+            }
+    
+            setUsePhoto(photoSelected.assets[0].uri)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    
     return (
         <VStack flex={1}>
             <ScreenHeader title="Perfil" />
 
-            
             <KeyboardAvoidingView 
                 behavior={Platform.OS === "ios" ? "padding" : "height"}
                 style={{ flex: 1 }}
@@ -31,12 +81,12 @@ export function Profile() {
                     >
                         <Center mt="$6" px="$10">
                             <UserPhoto 
-                                source={{ uri: "https://github.com/mayconsilva-dev.png"}} 
+                                source={{ uri: usePhoto }} 
                                 alt="Foto do usuário"
                                 size="xl"
                             />
 
-                            <TouchableOpacity>
+                            <TouchableOpacity onPress={handleUserPhotoSelect}>
                                 <Text 
                                     color="$green500"
                                     fontFamily="$heading"
